b2c265561f3b54f00503f9e76661ff530c0ab1ae
Georges Dubus <georges.dubus@compiletoi.net>
2011-07-06 10:30:50 +0200

    Correction de l'indentation des fractales grâce à /usr/lib/python2.7/Tools/scripts/reindent.py (le truc de feignasse).

--- 20110628-fractales/julia.py
+++ 20110628-fractales/julia.py
@@ -9,50 +9,49 @@ class julia_fract :
     def computePoint(self, pt, mode=1):
         n = 0
         if mode == 1:
-          z = pt
-          c = self.c
+            z = pt
+            c = self.c
         else:
-          z = 0.2 - 0.8j
-          c = pt
+            z = 0.2 - 0.8j
+            c = pt
         while n < self.depth:
-	   z = z*z + c
-           n+=1
-	   if z.real*z.real + z.imag*z.imag > 16:
- 	       return n
+            z = z*z + c
+            n+=1
+            if z.real*z.real + z.imag*z.imag > 16:
+                return n
         return n
-        
+
     def computePoint3(self, pt, mode=1):
         n = 0
         if mode == 1:
-          z = pt
-          c = self.c
+            z = pt
+            c = self.c
         else:
-          z = 0
-          c = pt
+            z = 0
+            c = pt
         while n < self.depth:
-           z = z*z*z + z*z + c
-           n+=1
-           if abs(z)>4:
-               return n
+            z = z*z*z + z*z + c
+            n+=1
+            if abs(z)>4:
+                return n
         return n
-        
+
     def computeJulia(self, size):
-      julia = zeros((size[0],size[1]))
-      for x in xrange(0, size[0]):
-        for y in xrange(0, size[1]):
-          z = complex(2*(float(x)/size[0])-1 , 3*(float(y)/size[1])-1.5)
-          #print z 
-          n = self.computePoint(z,size)
-          julia[x,y] = n
-      return julia
-    
+        julia = zeros((size[0],size[1]))
+        for x in xrange(0, size[0]):
+            for y in xrange(0, size[1]):
+                z = complex(2*(float(x)/size[0])-1 , 3*(float(y)/size[1])-1.5)
+                #print z
+                n = self.computePoint(z,size)
+                julia[x,y] = n
+        return julia
+
     def computeMandelbrot(self, size):
-      mandelbrot = zeros((size[0],size[1]))
-      for x in xrange(0, size[0]):
-        for y in xrange(0, size[1]):
-          z = complex(2*(float(x)/size[0])-1 , 3*(float(y)/size[1])-1.5)
-          #print z 
-          n = self.computePoint3(z, 0)
-          mandelbrot[x,y] = n
-      return mandelbrot
-      
+        mandelbrot = zeros((size[0],size[1]))
+        for x in xrange(0, size[0]):
+            for y in xrange(0, size[1]):
+                z = complex(2*(float(x)/size[0])-1 , 3*(float(y)/size[1])-1.5)
+                #print z
+                n = self.computePoint3(z, 0)
+                mandelbrot[x,y] = n
+        return mandelbrot
